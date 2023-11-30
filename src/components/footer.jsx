3dbad675bd7f49bffa57f67bd26964d69be59145
julio09 <jimmyjulio100@gmail.com>
2023-11-30 23:20:17 +0300

    suite animation

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,14 +1,36 @@
 import React from 'react'
 import { Avatar1, Avatar2, Avatar3, Bottom, Circle, Frame } from '../assets'
+import {motion} from 'framer-motion'
 
 function footer() {
+    const button = {
+        hidden: {
+          opacity: 0,
+        },
+        show: {
+          opacity: 1,
+          transition: {
+            duration: 0.9
+          }
+        },
+        hover: {
+          scale: 1.1,
+        },
+        tap: {
+          scale:0.8,
+          transition:{
+            type: "spring"
+          }
+        }
+      }
+
   return (
     <>
     <section className='flex justify-center relative items-center h-full mt-[90px]'>
     <hr className='border-[1px] w-[1440px] flex border-stroke shrink-0' />
     <div className='shrink-0 border-t-[2px]  absolute z-20 border-stroke bg-white w-[298px] h-[100px] rounded-[36px] '></div>
-    <button className='absolute z-30 px-[88px] py-[18px] rounded-radius font-bold font-sans text-normal bg-primary text-white '>Try to free</button>
-    <img src={Frame} alt='' className='absolute -bottom-[420px]' />
+    <motion.button variants={button} initial="hidden" whileTap="tap" animate="show" whileHover="hover" className='absolute z-30 px-[88px] py-[18px] rounded-radius font-bold font-sans text-normal bg-primary text-white '>Try to free</motion.button>
+    <motion.img initial={{translateY: 100, opacity:0}} animate={{translateY: 0, opacity: 1}} transition={{type: "spring", duration:.7,delay: 0.2,bounce:.4,}} src={Frame} alt='' className='absolute -bottom-[420px]' />
     <div className='flex flex-col absolute -bottom-[200px] left-56 items-start gap-[14px] '>
         <p className='font-sans font-bold text-[34px] text-primary'>Modern data stock</p>
         <p className='text-normal font-sans text-primary'>The investment you’ve made in every<br />
@@ -29,15 +51,15 @@ function footer() {
         <p className='text-normal'>Connect
         <br />More Experts</p>
     </div>
-    <div className='absolute -bottom-[420px] right-[425px] flex flex-col gap-2 items-start'>
+    <div className='absolute -bottom-[420px] flex right-[225px] gap-12 items-end'>
+        <div className='flex flex-col gap-2 items-end'>
         <span className='font-bold text-large'>120+</span>
         <p className='text-normal'>Perform complex,<br />
         ad hac analysis<br />
         and empower.</p>
-    </div>
-    <section className='flex absolute -bottom-[420px] right-56'>
+        </div>
         <img src={Bottom} alt=''/>
-    </section>
+    </div>
     </section>
     </>
   )
--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,21 +1,26 @@
-import React from 'react'
-import { Fleur, Sary } from '../assets'
+import { motion, useScroll } from "framer-motion";
+import React from 'react';
+import { Fleur, Sary } from '../assets';
+
 
 function hero() {
+
+  const { scrollYProgress } = useScroll;
+
   return (
-    <div className='relative flex flex-col justify-center items-center'>
+    <motion.div onScroll={scrollYProgress} className='relative flex flex-col justify-center items-center'>
         <hr className='border-[1px] w-[1440px] flex border-stroke shrink-0' />
     <div className=' border-stroke pt-10 relative font-bold text-center text-primary text-Xlarge font-sans leading-[130px] bg-[#FFF] '>Business Teams
     <br />
     Around
     <br />
     nalytical Work</div>
-    <img alt='' className='absolute left-[400px] top-52' width={246} height={76} src={Sary} />
-    <div className='absolute  right-[400px] top-52 rounded-radius bg-slate pl-[13px] pr-[31px] pt-[1px] flex items-center gap-[13px] '>
+    <motion.img initial={{translateX: -1000, opacity: 0}} animate={{translateX: 0, opacity:1}} transition={{duration:0.6, delay: .3, ease: "linear", type:"spring" ,stiffness: 50 }} alt='' className='absolute left-[400px] top-52' width={246} height={76} src={Sary} />
+    <motion.div initial={{translateX: 1000, opacity: 0}} animate={{translateX: 0, opacity:1}} transition={{duration:0.6, delay: .3, ease: "linear", type:"spring" ,stiffness: 50 }} className='absolute  right-[400px] top-52 rounded-radius bg-slate pl-[13px] pr-[31px] pt-[1px] flex items-center gap-[13px] '>
         <img alt='' width={75} height={75} src={Fleur} />
         <p className='font-normal text-start text-normal font-sans'>Behind every<br /> great idea</p>
-    </div>
-    </div>
+    </motion.div>
+    </motion.div>
   )
 }
 
